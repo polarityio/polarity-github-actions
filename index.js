feccc3461874ee67c435bae28dff420925ff7a9a
Jon Penwood <jonp@polarity.io>
2020-08-05 12:07:43 -0500

    Fixing action_file_names input format

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const fp = require('lodash/fp');
 
 const main = async () => {
   try {
     console.log('Starting Deploy Project Actions...\n');
     const projectId = core.getInput("project_id");
-    const actionFileNames = core.getInput("action_file_names");
+    const actionFileNames = fp.flow(
+      fp.split('\n'),
+      fp.trim
+    )(core.getInput('action_file_names'));
+    
     console.log(JSON.stringify({ projectId, actionFileNames }, null, 2));
   } catch (error) {
     core.setFailed(error.message);
