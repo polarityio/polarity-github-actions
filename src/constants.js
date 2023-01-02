251d025fd03f641e0c3c170fd55a75b4569cdaec
Jon Penwood <jonp@polarity.io>
2023-01-02 15:09:07 -0600

    Adding config.json file uploader, and refactored JS files to be in src directory

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+const REPO_BLOCK_LIST = [
+  'testing-github-actions',
+  'reference-channels',
+  'node-maxmind',
+  'polarityio.github.io',
+  'polarity-github-actions',
+  'polarity-integration-development-checklist',
+  'securonix'
+];
+
+const CONFIG_JSON_REPO_BLOCK_LIST = [
+
+]
+
+module.exports = {
+  REPO_BLOCK_LIST,
+  CONFIG_JSON_REPO_BLOCK_LIST
+};
---
+++ src/uploadActions.js
@@ -0,0 +1,76 @@
+const fs = require('fs');
+const { REPO_BLOCK_LIST } = require('./constants');
+const { filter, flow, includes, flatMap, map, get } = require('lodash/fp');
+
+const uploadActions = async (octokit, orgId, allOrgRepos, actionFileNames) => {
+  console.log('\nAction Files to Upload: ', actionFileNames, '\n');
+  
+  const fileCreationFunctions = flow(
+    filter(
+      (repo) => !includes(repo.name, REPO_BLOCK_LIST) && !repo.archived && !repo.disabled
+    ),
+    flatMap(getDeployFunctionsForActionFilesByRepo(octokit, orgId, actionFileNames))
+  )(allOrgRepos);
+
+  // Must run file creation in series due to the common use of the octokit instantiation
+  for (const fileCreationFunction of fileCreationFunctions) {
+    await fileCreationFunction();
+  }
+};
+
+const getDeployFunctionsForActionFilesByRepo =
+  (octokit, orgId, actionFileNames) =>
+  ({ name: repoName }) =>
+    map(
+      (actionFileName) => async () => {
+        const existingFileSha = await getExistingFileShaHash(
+          octokit,
+          orgId,
+          repoName,
+          actionFileName
+        );
+
+        await uploadActionFile(octokit, orgId, repoName, actionFileName, existingFileSha);
+
+        console.log(`- Action Upload Success: ${repoName} <- ${actionFileName}`);
+      },
+      actionFileNames
+    );
+
+const getExistingFileShaHash = async (octokit, orgId, repoName, actionFileName) =>
+  get(
+    'data.sha',
+    await octokit.repos
+      .getContent({
+        owner: orgId,
+        repo: repoName,
+        branch: 'develop',
+        path: `.github/workflows/${actionFileName}`
+      })
+      .catch((error) => {
+        if (!error.message.includes('Not Found')) {
+          throw error;
+        }
+      })
+  );
+
+const uploadActionFile = (octokit, orgId, repoName, actionFileName, existingFileSha) =>
+  octokit.repos.createOrUpdateFileContents({
+    owner: orgId,
+    repo: repoName,
+    path: `.github/workflows/${actionFileName}`,
+    message: `Uploading Github Action: ${actionFileName}`,
+    branch: 'develop',
+    ...(existingFileSha && { sha: existingFileSha }),
+    content: fs.readFileSync(actionFileName, 'base64'),
+    committer: {
+      name: orgId,
+      email: 'info@polarity.io'
+    },
+    author: {
+      name: orgId,
+      email: 'info@polarity.io'
+    }
+  });
+
+module.exports = uploadActions;
