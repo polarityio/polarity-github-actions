3e0b152fc2ac3399a0b1e3bfb8ecce69cb349d9e
Jon Penwood <jonp@polarity.io>
2023-03-09 18:00:41 -0600

    testing(arin): Testing deployment on arin

--- index.js
+++ index.js
@@ -9,6 +9,38 @@ const increasePackageJsonVersion = require('./src/increasePackageJsonVersion');
 const { createPullRequest, mergePullRequest } = require('./src/pullRequests');
 const addIntegrationIdToConfigs = require('./src/addIntegrationIdToConfigs');
 
+const main = async () => {
+  try {
+    console.info('Starting Deploy Organization Actions...\n');
+    const token = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(token);
+    
+    const orgId = core.getInput('org_id');
+    const actionFileNames = core.getMultilineInput('action_file_names');
+    const repoNamesForTesting = core.getMultilineInput('repository_names_for_testing');
+
+    const allOrgRepos = size(repoNamesForTesting)
+      ? map((name) => ({ name }), repoNamesForTesting)
+      : await getAllReposInOrg(octokit, orgId);
+
+    /** Add one-off functions to run here */
+    await addIntegrationIdToConfigs(octokit, orgId, allOrgRepos);
+
+    /** Feature Flagged Features */
+    if (core.getBooleanInput('increment_package_json_version'))
+      await increasePackageJsonVersion(octokit, orgId, allOrgRepos);
+
+    let createdPullRequests = [];
+    if (core.getBooleanInput('should_auto_create_pull_requests'))
+      createdPullRequests = await createPullRequest(octokit, orgId, allOrgRepos);
+
+    if (core.getBooleanInput('should_auto_merge_pull_requests'))
+      await mergePullRequest(octokit, orgId, createdPullRequests);
+  } catch (error) {
+    core.setFailed(error);
+  }
+};
+
 /** Useful Snippets
  * These are snippets that have been used in the past but aren't used often enough
  * to justify creating an Action File Input Flag (i.e. `core.getBooleanInput('increment_package_json_version')`)
@@ -22,42 +54,6 @@ const addIntegrationIdToConfigs = require('./src/addIntegrationIdToConfigs');
  * await uploadActions(octokit, orgId, allOrgRepos, actionFileNames);
  *
  */
-
-const main = async () => {
-  try {
-    console.info('Starting Deploy Organization Actions...\n');
-    const orgId = core.getInput('org_id');
-    const actionFileNames = core.getMultilineInput('action_file_names');
-    const incrementPackageJsonVersion = core.getBooleanInput('increment_package_json_version');
-    const shouldAutoCreatePullRequests = core.getBooleanInput(
-      'should_auto_create_pull_requests'
-    );
-    const shouldAutoMergePullRequests = core.getBooleanInput('should_auto_merge_pull_requests');
-    const repoNamesForTesting = core.getMultilineInput('repository_names_for_testing');
-
-    const token = core.getInput('GITHUB_TOKEN');
-    const octokit = github.getOctokit(token);
-
-    const allOrgRepos = size(repoNamesForTesting)
-      ? map((name) => ({ name }), repoNamesForTesting)
-      : await getAllReposInOrg(octokit, orgId);
-
-    /** Add one-off functions to run here */
-    await addIntegrationIdToConfigs(octokit, orgId, allOrgRepos);
-
-    if (incrementPackageJsonVersion)
-      await increasePackageJsonVersion(octokit, orgId, allOrgRepos);
-
-    let createdPullRequests = [];
-    if (shouldAutoCreatePullRequests)
-      createdPullRequests = await createPullRequest(octokit, orgId, allOrgRepos);
-
-    if (shouldAutoMergePullRequests)
-      await mergePullRequest(octokit, orgId, createdPullRequests);
-  } catch (error) {
-    core.setFailed(error);
-  }
-};
 
 main();
 
